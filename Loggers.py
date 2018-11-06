33a479e703cd1ac8af065140f1e08c5db67d5488
ahmedsoe <39407219+ahmedsoe@users.noreply.github.com>
2018-11-06 11:27:07 +0000

    Update and rename Loggers to Loggers.py

---
+++ Loggers.py
@@ -0,0 +1,20 @@
+#Logging
+logging.basicConfig(filename=r"path/to/logfile.log",
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger('logfilename')
+LOGGER.debug('Logger ready')
+
+#Logging2
+logger = logging.getLogger() # or getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+#Logging3 - Get all logs
+logger = logging.root # WORKS
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+logger = logging.getLogger('root') # DOES NOT WORK as you can't access the root logger from getLogger
+
+#Silencing Loggers:
+logging.getLogger("requests").setLevel(logging.WARNING) # or paramiko, or urllib3 (whatever library).
