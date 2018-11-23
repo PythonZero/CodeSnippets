791034e3302b209d6dc7714d3b7b406992487a0b
ahmedsoe <39407219+ahmedsoe@users.noreply.github.com>
2018-11-23 17:11:19 +0000

    Create Pandas Dataflows.py

---
+++ Pandas Dataflows.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+raw_df = pd.DataFrame(...) #i.e. 10 rows x 4 columns
+
+def create_empty_df(raw_df):
+    """Create a dataframe of same rows as the raw file"""
+    empty_array = np.full((len(raw_df), 1), np.nan)
+    df = pd.DataFrame(empty_array)
+    return df
+
+def fill_empty_df(raw_df, empty_df)
+    df = empty_df
+    df['colname'] = raw_df[raw_df['col1'] == 'condition']['col8'] # only fills certain rows with the data in col8
+    df['GSP'] = raw_df[raw_df['col0'] == 'GSP']['col4'] 
+    # Checks where the row col0 is GSP, and takes the GSP from column 4.
