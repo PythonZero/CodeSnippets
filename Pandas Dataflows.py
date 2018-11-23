412cbcc3a6bb7869b1c6b597312b4e48bd29a73a
ahmedsoe <39407219+ahmedsoe@users.noreply.github.com>
2018-11-23 17:11:40 +0000

    Update Pandas Dataflows.py

--- Pandas Dataflows.py
+++ Pandas Dataflows.py
@@ -8,8 +8,9 @@ def create_empty_df(raw_df):
     df = pd.DataFrame(empty_array)
     return df
 
-def fill_empty_df(raw_df, empty_df)
+def fill_empty_df_dataflow_name(raw_df, empty_df)
     df = empty_df
     df['colname'] = raw_df[raw_df['col1'] == 'condition']['col8'] # only fills certain rows with the data in col8
     df['GSP'] = raw_df[raw_df['col0'] == 'GSP']['col4'] 
     # Checks where the row col0 is GSP, and takes the GSP from column 4.
+    return df
