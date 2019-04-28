af5b741c4b8c591a82871f7e970715040d79797c
PythonZero <PythonZero@users.noreply.github.com>
2019-04-28 13:36:33 +0100

    Create DataClasses.py

---
+++ DataClasses.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataSoldier:
+    name: str
+    attack: int
+    defence: int
+    hp: int
+
+
+class Soldier:
+    def __init__(self, name, attack, defence, hp):
+        self.name = name
+        self.attack = attack
+        self.defence = defence
+        self.hp = hp
+
+
+x = DataSoldier('Bob', 10, 20, 300)
+y = Soldier('Jack', 10, 20, 300)
+
